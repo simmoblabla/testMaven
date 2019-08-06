f3f516bd41d99b8d8f6336be5f38fcaad4ec48d8
Simone Sapienza <simone.sapienza@otconsulting.com>
2019-08-06 14:27:59 +0200

    Initial commit

---
+++ src/main/java/Prova.java
@@ -0,0 +1,14 @@
+public class Prova {
+    public static int somma(int a, int b){
+        return (a+b);
+    }
+    public static int sottrai(int a, int b){
+        return (a-b);
+    }
+    public static int moltiplica(int a, int b){
+        return (a*b);
+    }
+    public static int dividi(int a, int b){
+        return (a/b);
+    }
+}
